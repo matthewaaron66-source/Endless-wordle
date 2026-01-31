f11fff271511577a5e2e6df2ca9ba12184c1453e
matthewaaron66-source <matthewaaron66@gmail.com>
2026-01-30 22:05:42 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "endless-worldle-v5";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon.svg",
+  "./assets/css/styles.css",
+  "./assets/js/app.js",
+  "./assets/js/stats.js",
+  "./assets/js/aliases.js",
+  "./data/countries-10m.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k)))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((resp) => {
+        if (event.request.method === "GET") {
+          const copy = resp.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
